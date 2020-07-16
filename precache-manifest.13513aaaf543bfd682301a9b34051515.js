4f5ace39024b61414aad2e530a76c682d9428a4f
stolzz0613 <andres0613@utp.edu.co>
2020-07-16 00:02:03 -0500

    Updates

---
+++ precache-manifest.13513aaaf543bfd682301a9b34051515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d46af2939267e08bc9e7b3a829f5be7",
+    "url": "/Seguros/index.html"
+  },
+  {
+    "revision": "481056a475dbcc7ea13b",
+    "url": "/Seguros/static/css/main.722465ce.chunk.css"
+  },
+  {
+    "revision": "9fccd8313fb1d2915815",
+    "url": "/Seguros/static/js/2.6dbb9dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Seguros/static/js/2.6dbb9dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481056a475dbcc7ea13b",
+    "url": "/Seguros/static/js/main.962e0ec3.chunk.js"
+  },
+  {
+    "revision": "afcd77231085e6657ee6",
+    "url": "/Seguros/static/js/runtime-main.026edbd0.js"
+  }
+]);
